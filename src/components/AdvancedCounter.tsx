8d88e53d7b154c5a0cc1b96b92c74b9bc25dee20
Albina93 <alba.tleubergen@gmail.com>
2026-05-02 01:40:07 -0400

    Added keyboard arrow key controls

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,23 +1,55 @@
 import { useState, useEffect } from "react";
 
 function AdvancedCounter() {
-  const [count, setCount] = useState(() => {
+  const [count, setCount] = useState<number>(() => {
     const saved = localStorage.getItem("count");
     return saved ? Number(saved) : 0;
   });
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState<number>(() => {
+    const saved = localStorage.getItem("step");
+    return saved ? Number(saved) : 1;
+  });
   const [history, setHistory] = useState<number[]>(() => {
     const saved = localStorage.getItem("history");
-    console.log("Loading history from localstorage: ", saved);
+    // console.log("Loading history from localstorage: ", saved);
     return saved ? JSON.parse(saved) : [];
   });
 
   useEffect(() => {
     localStorage.setItem("count", String(count));
   }, [count]);
+
+  useEffect(() => {
+    localStorage.setItem("step", String(step));
+  }, [step]);
+
   useEffect(() => {
     localStorage.setItem("history", JSON.stringify(history));
   }, [history]);
+
+  useEffect(() => {
+    // define the function that handles the key press
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // console.log("Key pressed: ", event.key);
+
+      // if event.key is "ArrowUp" then call handleIncrement
+      if (event.key === "ArrowUp") {
+        handleIncrement();
+      }
+      // if event.key is "ArrowDown" then call handleDecrement
+      if (event.key === "ArrowDown") {
+        handleDecrement();
+      }
+    };
+    // tell the browser to start listening
+    document.addEventListener("keydown", handleKeyDown);
+
+    // return a cleanup function that stops the listener
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [count, step]);
+
   const handleIncrement = () => {
     const newCount = count + step;
     setCount(newCount);
